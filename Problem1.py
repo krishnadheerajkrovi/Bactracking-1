e97047446235abe1a55a063a185fa2c942a88dd0
Krishna Dheeraj Krovi <krishnadheeraj22@gmail.com>
2024-06-27 13:38:12 -0700

    Added time and space complexities

--- Problem1.py
+++ Problem1.py
@@ -3,6 +3,9 @@
 2. Recurse with same index and target - current number. Once a path is found, we backtrack and try with the next number. 
 3. We break from a path if the target becomes negative. If target becomes 0, we have found a valid path and we add it to the result.
 
+N = len(candidates), M = target, min_cand = min(candidates)
+TC: O(N^(M/min_cand)) Since each candidate can produce a max depth of M/min_candidate length path and we have n such candidates
+SC: O(M/min_cand) - recursion depth
 '''
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
--- Problem2.py
+++ Problem2.py
@@ -3,6 +3,9 @@
 2. Each number can be a single digit or multiple digits. We use an index to keep track of the number we are currently processing.
 3. A calc variable is used to keep track of the current value of the expression. A tail variable is used to keep track of the previous value of the expression for multiplication.
 4. If the expression evaluates to the target, we add it to the result.
+
+TC: O(4^N)       N-1 places between N numbers and can be one of 3 operations - 3^(N-1) and another linear operation proportional to string length N to compute the arithmetic expression
+SC: O(N)         Space for the case where expression includes all numbers and operations
 '''
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
